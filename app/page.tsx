b52d9caeefc4930d0eaf4444918212cd4bc3d8fe
Ulises Pla Rocher <ulises.pla@innova-tsn.com>
2025-08-26 21:10:24 +0200

    fffffdd

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen gap-24 p-6">
       <h1 className="text-2xl font-semibold mb-4 mt-32">You can Drag and Drop Me!</h1>
-
-     
       <PiledSquares />
     </div>
   );
 }
+     
+
